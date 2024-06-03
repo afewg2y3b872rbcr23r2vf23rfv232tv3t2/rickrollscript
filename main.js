b034b12a646ffa241da69d47b9be71dc6dfe23f6
afewg2y3b872rbcr23r2vf23rfv232tv3t2 <glasnak.mmichal@gmail.com>
2024-06-03 16:56:21 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-<script src="https://yonle.github.io/RickRoll.js/RickRoll.js"></script>
-<script>
-  rickroll()
-  rickroll.showup()
-</script>
+// Create a new script element
+var script = document.createElement('script');
+
+// Set the type attribute
+script.type = 'text/javascript';
+
+// Set the source of the script to the URL
+script.src = 'https://yonle.github.io/RickRoll.js/RickRoll.js';
+
+// Add an onload event handler to call the functions once the script is loaded
+script.onload = function() {
+    rickroll();
+    rickroll.showup();
+};
+
+// Append the script element to the document head
+document.head.appendChild(script);
